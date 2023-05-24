c3b0ebddd5c27773ee89bf31c01108836e8d966a
Maria Cosmen <mcosme00@gmail.com>
2023-05-24 20:19:54 +0900

    Create toggle element and switch between themes

--- src/themes.js
+++ src/themes.js
@@ -1,3 +1,22 @@
 export function handleThemes() {
-  console.log("Hello from themes!!!!")
+  let inputs = document.querySelectorAll("input");
+  const body = document.querySelector("body");
+  inputs = [...inputs]
+
+  function changeStyle(input) {
+    console.log(`Selected input: ${input.id}`)
+    console.log('Changing the styles!!')
+    body.className = '';
+    body.classList.add(`${input.id}-theme`)
+  }
+
+
+  inputs.forEach(input => {
+    input.addEventListener('click', () => {
+      input.style.opacity = '1';
+      changeStyle(input);
+      const different = inputs.filter(item => item !== input)
+      different.forEach(diff => diff.style.opacity = '0')
+    })
+  })
 }
